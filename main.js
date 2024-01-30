c23af278d38408a51a35b1f17f530c285d33ba32
Jhefferson Araújo <110609799+JhefAraujo@users.noreply.github.com>
2024-01-29 22:04:29 -0300

    english CV

--- main.js
+++ main.js
@@ -1,4 +1,14 @@
-$('input').on('change', function() {
-    $('body').toggleClass('blue');
-  });
+
+
+  window.addEventListener('scroll', function() {
+    var scrollTopPosition = window.scrollY;
+
+    if (scrollTopPosition > 90) {
+      this.document.getElementsByClassName('logo')[0].style.transform = 'none'
+    }
+    if (scrollTopPosition < 90) {
+      this.document.getElementsByClassName('logo')[0].style.transform = 'translateX(-70px)'
+    }
   
+    console.log('Posição vertical do scroll:', scrollTopPosition);
+  });
