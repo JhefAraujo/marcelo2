52fac50009f4d665a9ece28b3bd58ea3c3f5cfc6
Jhefferson Araújo <110609799+JhefAraujo@users.noreply.github.com>
2024-01-29 22:12:12 -0300

    hotfix

--- main.js
+++ main.js
@@ -2,13 +2,15 @@
 
   window.addEventListener('scroll', function() {
     var scrollTopPosition = window.scrollY;
+    if (this.window.innerWidth < 500) {
+      if (scrollTopPosition > 90) {
+        this.document.getElementsByClassName('logo')[0].style.transform = 'none'
+      }
+      if (scrollTopPosition < 90) {
+        this.document.getElementsByClassName('logo')[0].style.transform = 'translateX(-70px)'
+      }
+    }
 
-    if (scrollTopPosition > 90) {
-      this.document.getElementsByClassName('logo')[0].style.transform = 'none'
-    }
-    if (scrollTopPosition < 90) {
-      this.document.getElementsByClassName('logo')[0].style.transform = 'translateX(-70px)'
-    }
   
     console.log('Posição vertical do scroll:', scrollTopPosition);
   });
